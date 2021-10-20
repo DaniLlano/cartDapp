aa8004ca4e18cd440705a32d3c843367472406ef
Dani <danillano97@gmail.com>
2021-10-19 23:39:08 -0300

    initial commit

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+contract Marketplace {
+    string public name;
+    int public productCount = 0;
+    mapping(uint => Product) public products;
+    
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
+    constructor() public {
+        name = "Dapp University Marketplace";
+    }
+
+}
