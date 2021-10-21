1f2a0bd9b52b5bc859481a98cbf8db8061e732f8
Dani <danillano97@gmail.com>
2021-10-20 23:47:29 -0300

    create product

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -13,8 +13,62 @@ contract Marketplace {
         bool purchased;
     }
 
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
+        bool purchased
+    );
+
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // require a valid name
+        require(bytes(_name).lenght > 0);
+        // require a valid price
+        require(_price > 0);
+        // increment product count
+        productCount++;
+        // create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false)
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        // fetch the product
+        Product memory _product = products[id];
+        // fetch the owner
+        address payable _seller = product.owner;
+        // make sure the product has a valid id
+        require(_product.id > && _product.id <= productCount);
+        // require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // require that the product has not been purchased already
+        require(!_product.purchased);
+        // require that the buyer is not the seller
+        require(seller != msg.sender);
+        // transfer ownership to the buyer
+        _product.owner = msg.sender;
+        // mark as purchased
+        _product.purchased = true;
+        // update the product
+        products[_id] = _product;
+        // pay the seller by sending them Ether
+        address(_seller.transfer(msg.value));
+        // trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
     }
 
 }
